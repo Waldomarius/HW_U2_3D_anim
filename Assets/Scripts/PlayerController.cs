030b49e7743bbb15101a058b577decc13bab496e
Waldomarius <40439764+Waldomarius@users.noreply.github.com>
2026-01-15 00:13:49 +0300

    2026.01.15 Add new input sustem.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,72 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 _moveInput;
+    private Rigidbody _rb;
+    
+    private PlayerInput _playerInput;
+    private InputAction _moveAction;
+    private InputAction _jumpAction;
+
+    [Header("Moving options")]
+    [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private float _jumpForce = 5f;
+
+    
+    void Awake()
     {
+        _rb = GetComponent<Rigidbody>();
+        _playerInput = GetComponent<PlayerInput>();
         
+        _moveAction = _playerInput.actions["Moved"];
+        _jumpAction = _playerInput.actions["Jump"];
+        
+         // _moveAction = InputSystem.actions.FindAction("Player/Move");
+         // _jumpAction = InputSystem.actions.FindAction("Player/Jump");
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        _moveAction.Enable();
+        _jumpAction.Enable();
+            
+        _moveAction.performed += OnMovePerformed;
+        _moveAction.canceled += OnMoveCanceled;
+        _jumpAction.performed += OnJumpPerformed;
+    }
+
+    private void OnDisable()
+    {
+        _moveAction.Disable();
+        _jumpAction.Disable();
+            
+        _moveAction.performed -= OnMovePerformed;
+        _moveAction.canceled -= OnMoveCanceled;
+        _jumpAction.performed -= OnJumpPerformed;
+    }
+
+
+    private void OnMovePerformed(InputAction.CallbackContext context)
+    {
+        _moveInput = context.ReadValue<Vector2>();
+    }
+    
+    private void OnMoveCanceled(InputAction.CallbackContext context)
+    {
+        _moveInput = Vector2.zero;
+    }
+    private void OnJumpPerformed(InputAction.CallbackContext context)
+    {
+        _rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+    }
+
+
+    private void FixedUpdate()
+    {
+        Vector3 move = new Vector3(_moveInput.x, 0, _moveInput.y) * (_moveSpeed * Time.fixedDeltaTime);
+        _rb.MovePosition(transform.position + move);
     }
 }
